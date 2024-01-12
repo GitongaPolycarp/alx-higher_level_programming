a21996b6e4d0d2d43ece6e5c77d67b8f7c523c3f
POLYCARP <memepolycarp@gmail.com>
2024-01-12 22:32:11 +0300

    update_dictionary

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,1 +1,15 @@
-#!/usr/bin/python3 def update_dictionary(a_dictionary, key, value):     a_dictionary[key]
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    """Updates a dictionary by replacing or adding key/value pairs.
+
+    Args:
+        a_dictionary: The dictionary to update.
+        key: The key to update or create.
+        value: The new value to assign to the key.
+
+    Returns:
+        The updated dictionary.
+    """
+
+    a_dictionary[key] = value
+    return (a_dictionary)
